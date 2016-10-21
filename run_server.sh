02c91f8a0d720148d943082fbbe0b1ef7f253332
Peter Dodds <peter@pddds.com>
2016-10-21 19:52:01 +0100

    Wait for the server to start before opening it in the browser

--- run_server.sh
+++ run_server.sh
@@ -2,6 +2,8 @@
 
 start cmd /c"bundler exec jekyll server --watch"
 
-sleep 1
+# wait for server to start
+while ! curl http://localhost:4000/ &>/dev/null; do sleep 1; done
 
+# open the homepage
 explorer http://localhost:4000/
