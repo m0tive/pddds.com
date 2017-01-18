79bd4790cd0103f3aaaf9857607c805717e60136
Peter Dodds <peter@pddds.com>
2017-01-18 20:23:37 +0000

    Load the post list from a json file

--- js/firehose.js
+++ js/firehose.js
@@ -1,9 +1,29 @@
 // ~
 
 function buildFirehose(id) {
-    let item = document.createElement("li");
-    item.innerText = "Hello world";
+    // Load the posts json
+    let request = new XMLHttpRequest();
+    request.open('Get', '/data/posts.json', true/*async*/);
 
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            let data = JSON.parse(this.response);
+            onPostsLoaded(id, data.posts);
+        } else {
+            // TODO
+        }
+    };
+
+    // TODO
+    //request.onerror = function() { };
+    request.send();
+}
+
+function onPostsLoaded(id, posts) {
     let list = document.getElementById(id);
-    list.appendChild(item);
+    for (let post of posts) {
+        let item = document.createElement("li");
+        item.innerText = post.title;
+        list.appendChild(item);
+    }
 }
