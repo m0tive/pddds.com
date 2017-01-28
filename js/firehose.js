e848214eae72b007ca70a069dbf2ebbae89ab0cb
Peter Dodds <peter@pddds.com>
2017-01-28 19:46:06 +0000

    Use single function to add items

--- js/firehose.js
+++ js/firehose.js
@@ -1,7 +1,21 @@
 // ~
 "use strict";
 
+let LIST_ID = "";
+
+function addPost(time, data) {
+    // loop over items, add them if missing, remove if there's too many
+
+    let list = document.getElementById(LIST_ID);
+    let item = document.createElement("li");
+    item.id = time.toString();
+    item.innerHTML = data.innerHTML;
+    list.appendChild(item);
+}
+
 function buildFirehose(id) {
+    LIST_ID = id;
+
     // Load the posts json
     let request = new XMLHttpRequest();
     request.open('Get', '/data/posts.json', true/*async*/);
@@ -22,41 +36,58 @@ function buildFirehose(id) {
     getScript(SITE_BASEURL + "/js/twitterFetcher.js", function() {
 
         twitterFetcher.fetch({
-          "profile": {"screenName": 'pdddddds'},
-          "dataOnly": true,
-          "showImages": true,
-          "customCallback": function(tweets) {
-              let list = document.getElementById(id);
-              for (let post of tweets) {
-                  // TODO - use data-expanded-url instead of href on anchors
-                  let item = document.createElement("li");
-                  item.className = "tweet";
-                  item.innerHTML = post.tweet;
-                  list.appendChild(item);
+            "profile": {"screenName": 'pdddddds'},
+            "dataOnly": true,
+            "showImages": true,
+            "customCallback": function(tweets) {
+                //let list = document.getElementById(id);
+                for (let post of tweets) {
+                    addPost(new Date(post.timestamp).getTime(), {
+                        "className": "tweet",
+                        "innerHTML": post.tweet,
+                        "image": post.image
+                    });
 
-                  if (post.image !== undefined)
-                  {
-                      let imageFrame = document.createElement("div");
-                      imageFrame.style = "width: 100px; height: 100px; overflow: hidden;";
+                    /*
+                    // TODO - use data-expanded-url instead of href on anchors
+                    let item = document.createElement("li");
+                    item.className = "tweet";
+                    item.innerHTML = post.tweet;
+                    list.appendChild(item);
 
-                      let image = document.createElement("img");
-                      image.src = post.image;
-                      image.style = "width: 100%;";
-                      imageFrame.appendChild(image);
+                    if (post.image !== undefined)
+                    {
+                        let imageFrame = document.createElement("div");
+                        imageFrame.style = "width: 100px; height: 100px; overflow: hidden;";
 
-                      item.appendChild(imageFrame);
-                  }
-              }
-          },
+                        let image = document.createElement("img");
+                        image.src = post.image;
+                        image.style = "width: 100%;";
+                        imageFrame.appendChild(image);
+
+                        item.appendChild(imageFrame);
+                    }
+                    */
+                }
+            },
         });
     });
 }
 
 function onPostsLoaded(id, posts) {
-    let list = document.getElementById(id);
+    //let list = document.getElementById(id);
     for (let post of posts) {
+        let date = new Date(post.date);
+        addPost(date.getTime(), {
+            "className": "tweet",
+            "innerHTML": post.title,
+            "image": post.image
+        });
+
+        /*
         let item = document.createElement("li");
         item.innerText = post.title;
         list.appendChild(item);
+        */
     }
 }
